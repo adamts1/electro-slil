657b5a03e774afa88ff985d9b19524dad65adb1e
Adam Tsityat <adamtsityat@gmail.com>
2025-12-27 20:17:46 +0200

    last change

--- src/components/WhatsAppDemo/WhatsAppInterface.tsx
+++ src/components/WhatsAppDemo/WhatsAppInterface.tsx
@@ -382,12 +382,6 @@ export const WhatsAppInterface = ({
                   </svg>
                 </button>
                 <button className="p-1 md:p-1.5 text-slate-600 hover:text-slate-800">
-                  <svg className="w-4 h-4 md:w-5 md:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                  </svg>
-                </button>
-                <button className="p-1 md:p-1.5 text-slate-600 hover:text-slate-800">
                   <svg className="w-4 h-4 md:w-5 md:h-5" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z" />
                   </svg>
